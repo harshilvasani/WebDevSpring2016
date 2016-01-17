52c77e07ae995ab96f511a9699df7efd3811f135
harshilvasani <vasani.h@husky.neu.edu>
2016-01-17 12:48:35 -0500

    new server added

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 #!/bin/env node
 //  OpenShift sample Node application
 var express = require('express');
-var app      = express();
+var app = express();
 
-app.use(express.static(__dirname + '/public'));
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
-app.get('/say/hello',sayHello);
-function sayHello(req,res){
-    res.send('hello everyone');
-}
+app.get('/hello', function(req, res){
+    res.send('hello world');
+});
+
+app.listen(port, ipaddress);
 
 app.get('/api/users', function (req,res) {
    var users = [
