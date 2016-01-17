58a3818000e2a0d74c1b969e50c8fb345fdeb258
harshilvasani <vasani.h@husky.neu.edu>
2016-01-17 12:51:59 -0500

    new server added

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ var app = express();
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
+
+app.get('/', function(req, res){
+    res.send('hello my friend');
+});
+
 app.get('/hello', function(req, res){
     res.send('hello world');
 });
