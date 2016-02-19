bbe3383dc8c4b63a0b3131ec818abeaa7480f0d5
harshilvasani <vasani.h@husky.neu.edu>
2016-02-19 18:27:34 -0500

    made till working login and registration controller

---
+++ public/assignment/config.js
@@ -0,0 +1,40 @@
+(function(){
+    angular
+        .module("FormBuilderApp")
+        .config(Configure);
+
+    function Configure($routeProvider) {
+        $routeProvider
+            .when("/home",{
+                templateUrl: "../assignment/views/home/home.view.html"
+               // controller
+            })
+            .when("/register", {
+                templateUrl: "../assignment/views/users/register.view.html",
+                controller: "RegisterController"
+            })
+            .when("/login", {
+                templateUrl: "../assignment/views/users/login.view.html",
+                controller : "LoginController"
+            })
+            .when("/profile", {
+                templateUrl: "../assignment/views/users/profile.view.html"
+              //  controller
+            })
+            .when("/admin", {
+                templateUrl: "../assignment/views/admin/admin.view.html"
+              //  controller
+            })
+            .when("/forms", {
+                templateUrl: "../assignment/views/forms/forms.view.html"
+                //  controller
+            })
+            .when("/form-fields", {
+                templateUrl: "../assignment/views/forms/form-fields.view.html"
+                //  controller
+            })
+            .otherwise({
+                redirectTo: "/home"
+            });
+    }
+})();
---
+++ public/assignment/services/user.service.client.js
@@ -0,0 +1,68 @@
+/**
+ * Created by Harshin on 19-Feb-16.
+ */
+
+(function () {
+    angular
+        .module("FormBuilderApp")
+        .factory("UserService",UserService);
+
+    function UserService() {
+        var users = [
+            {	"_id":123, "firstName":"Alice",            "lastName":"Wonderland",
+                "username":"alice",  "password":"alice",   "roles": ["student"]		},
+            {	"_id":234, "firstName":"Bob",              "lastName":"Hope",
+                "username":"bob",    "password":"bob",     "roles": ["admin"]		},
+            {	"_id":345, "firstName":"Charlie",          "lastName":"Brown",
+                "username":"charlie","password":"charlie", "roles": ["faculty"]		},
+            {	"_id":456, "firstName":"Dan",              "lastName":"Craig",
+                "username":"dan",    "password":"dan",     "roles": ["faculty", "admin"]},
+            {	"_id":567, "firstName":"Edward",           "lastName":"Norton",
+                "username":"ed",     "password":"ed",      "roles": ["student"]		}
+        ]
+
+
+        var api = {
+            findUserByUsernameAndPassword : findUserByUsernameAndPassword,
+            findAllUsers : findAllUsers,
+            createUser : createUser,
+            deleteUserById : deleteUserById,
+            updateUser : updateUser
+        }
+
+        return api;
+
+        function findUserByUsernameAndPassword(username, password, callback) {
+            var flag=false;
+           for(i in users){
+                if(users[i].username==username && users[i].password==password){
+                callback(users[i]);
+                    flag=true;
+               }
+            }
+
+            if (flag==false){
+                callback(null);
+            }
+        }
+
+        function findAllUsers(callback) {
+            callback(users);
+        }
+
+        function createUser(user, callback) {
+            users.push(user);
+            alert(user.username);
+            callback(user);
+        }
+
+        function deleteUserById(userId, callback) {
+
+        }
+
+        function updateUser(userId, user, callback) {
+
+        }
+
+    }
+})();
