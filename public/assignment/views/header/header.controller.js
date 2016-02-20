76a3e14965dfb7d8eac172d9aac0d041cd745daa
harshilvasani <vasani.h@husky.neu.edu>
2016-02-19 22:29:08 -0500

    implemented header's event handlers

---
+++ public/assignment/views/header/header.controller.js
@@ -0,0 +1,45 @@
+(function(){
+    angular
+        .module("FormBuilderApp")
+        .controller("HeaderController", HeaderController);
+
+    function HeaderController($scope,UserService) {
+
+        $rootScope=null;
+
+        $scope.checkRootScope=checkRootScope;
+        $scope.updateRootScope=updateRootScope;
+        $scope.isAdmin=isAdmin;
+
+        function checkRootScope() {
+            if($rootScope==null){
+                return true;
+            }
+            else{
+                $scope.username=$rootScope.username;
+                return false;
+            }
+        }
+
+        function updateRootScope() {
+
+            $rootScope=null;
+
+        }
+        function isAdmin(){
+            if($rootScope!=null)
+            {
+                var roles=$rootScope.roles;
+                if(roles!=null) {
+                    for (var i in roles) {
+                        if (roles[i] == "admin") {
+                            return true;
+                            alert();
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+    }
+})();
