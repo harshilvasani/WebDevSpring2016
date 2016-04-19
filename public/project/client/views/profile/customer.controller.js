555f09529ce157498bd9ae5accd6a9ec362aa9a2
harshilvasani <vasani.h@husky.neu.edu>
2016-04-19 02:30:22 -0400

    updated header's css

--- public/project/client/views/profile/customer.controller.js
+++ public/project/client/views/profile/customer.controller.js
@@ -6,7 +6,7 @@
         .module("VehicleBookingApp")
         .controller("CustomerProfileController", CustomerProfileController);
 
-    function CustomerProfileController(UserService,CustomerProfileService) {
+    function CustomerProfileController(UserService) {
 
       //  alert("in Customer's profile");
 
@@ -15,13 +15,8 @@
         UserService
             .getCurrentUser()
             .then(function (res){
-               // console.log(res.data);
+                //console.log(res.data);
                 vm.curCustomer = res.data;
-
-               // var img = document.getElementById('custImg');
-                //img.src = 'data:image/*;base64,' + btoa(vm.curCustomer.img.data); //JS have btoa() function for it.
-                //img.src = vm.curCustomer.img.data;
-                //img.src = "images/user.png";
             });
     }
 
